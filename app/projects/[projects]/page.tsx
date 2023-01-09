c7fc94ebd4d3cab1b028ad55d7ab3d9fa2ee22c3
muzaffer <muzafferckaplan@gmail.com>
2023-01-09 22:22:28 +0300

    projects page projects detail page css design changed

--- app/projects/[projects]/page.tsx
+++ app/projects/[projects]/page.tsx
@@ -2,59 +2,83 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
+import { useMemo } from "react";
 import { MdArrowForwardIos } from "react-icons/md";
 import { VscDebugBreakpointLog } from "react-icons/vsc";
 import { projectsModal } from "../../../data/projectsModal";
+import { projectModal } from "../../../typings";
 
 const Projects = () => {
   const searchParams = useSearchParams();
   const page = searchParams.get("id");
+  const currentProject = useMemo<projectModal | undefined>(() => {
+    return page
+      ? projectsModal.find((item) => item.id === Number(page))
+      : undefined;
+  }, [page]);
   return (
     <>
-      {projectsModal
-        .filter((item: any) => item.id == page)
-        .map((item) => (
-          <div key={item.id} className="relative">
-            <div className="w-screen h-[50vh] relative">
-              <div className="group tracking-[.5rem] text-[#66FCF1] uppercase cursor-pointer border border-[#45A29E] absolute z-50 left-10 top-10">
-                <Link
-                  href="/projects"
-                  className="px-4 py-1 flex items-center gap-1"
-                >
-                  <MdArrowForwardIos
-                    className="group-hover:rotate-180 duration-500"
-                    size={16}
-                  />
-                  Back
-                  <MdArrowForwardIos className="rotate-180 -ml-2" size={16} />
-                </Link>
-              </div>
-              <div className="absolute top-0 left-0 w-full h-[50vh] bg-primary opacity-50 z-10" />
-              <Image
-                className="absolute z-1"
-                layout="fill"
-                objectFit="cover"
-                src="/Avatar.jpg"
-                alt="/"
+      <div key={currentProject?.id} className="relative">
+        <div className="w-screen h-[35vh] md:h-[50vh] relative">
+          <div className="group tracking-[.5rem] text-[#66FCF1] uppercase cursor-pointer border border-[#45A29E] absolute z-50 left-10 top-10">
+            <Link
+              href="/projects"
+              className="px-4 py-1 flex items-center gap-1"
+            >
+              <MdArrowForwardIos
+                className="group-hover:rotate-180 duration-500"
+                size={16}
               />
-              <div className="absolute top-[70%] ml-4 max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
-                <h2 className="py-2 text-left text-2xl"> {item.title} </h2>
-                <div className="md:flex gap-2">
-                  {item.dependicies.map((depen: string) => (
-                    <h3 key={depen} className="flex items-center gap-1">
-                      <VscDebugBreakpointLog color="lightblue" size={12} />
-                      {depen}
-                    </h3>
-                  ))}
-                </div>
-              </div>
-            </div>
-            <div className="text-left mx-20 mt-10 mb-10">
-              <h3 className="mb-4 text-2xl">Project</h3>
-              <p className=" leading-7 "> {item.description} </p>
+              Back
+              <MdArrowForwardIos
+                className="rotate-180 group-hover:rotate-[540deg] duration-500 -ml-2"
+                size={16}
+              />
+            </Link>
+          </div>
+          <div className="absolute top-0 left-0 w-full h-[35vh] md:h-[50vh] bg-primary opacity-50 z-10" />
+          <Image
+            className="absolute z-1"
+            layout="fill"
+            src={currentProject?.image || ""}
+            alt="/"
+          />
+          <div className="absolute top-[70%] mx-20 max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
+            <h2 className="py-2 text-left text-2xl">
+              {" "}
+              {currentProject?.title}{" "}
+            </h2>
+            <div className="md:flex gap-2 mb-2 md:mb-0">
+              {currentProject?.dependicies.map((depen: string) => (
+                <h3 key={depen} className="flex items-center gap-1 capitalize">
+                  <VscDebugBreakpointLog color="lightblue" size={12} />
+                  {depen}
+                </h3>
+              ))}
             </div>
           </div>
-        ))}
+        </div>
+        <div className="text-left mx-20 mt-10 mb-5 md:mb-10">
+          <h3 className="mb-4 text-2xl">Project</h3>
+          <p className=" leading-7 "> {currentProject?.description} </p>
+        </div>
+        <div className="space-x-5 mb-10">
+          <Link
+            target="_blank"
+            href={currentProject?.demoUrl || ""}
+            className="border-border border p-2 tracking-widest font-serif rounded-md hover:bg-border"
+          >
+            Demo
+          </Link>
+          <Link
+            target="_blank"
+            href={currentProject?.githubUrl || ""}
+            className="border-border border p-2 tracking-widest font-serif rounded-md hover:bg-border"
+          >
+            Github
+          </Link>
+        </div>
+      </div>
     </>
   );
 };
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import { projectsModal } from "../../data/projectsModal";
 
@@ -9,30 +10,26 @@ const page = () => {
         {projectsModal.map((item) => (
           <div
             key={item.id}
-            className="group relative text-2xl rounded-lg flex items-center justify-center w-full h-40 border border-border"
-            style={{
-              backgroundImage: `url(${item.image})`,
-              backgroundSize: "cover",
-            }}
+            className="group overflow-hidden relative text-2xl rounded-lg flex flex-col items-center justify-center w-full h-40 border border-border"
           >
+            <p className="z-10 absolute top-2">{item.title}</p>
+            <Image
+              className="absolute z-1 rounded-lg group-hover:scale-125 duration-300"
+              layout="fill"
+              src={item.image}
+              alt="/"
+            />
             <p className="group-hover:hidden hidden">{item.title}</p>
-            <div className="hidden group-hover:block w-full h-full bg-primary absolute opacity-50 rounded-lg border-primary" />
+            <div className="hidden group-hover:block w-full h-full bg-primary absolute opacity-80 rounded-lg border-primary" />
             <div className="flex items-center gap-5 z-50 group-hover:flex">
               <Link
                 href={{
                   pathname: `/projects/${item.title}`,
                   query: { id: item.id },
                 }}
-                className="border-border border p-2 rounded-md hover:bg-border"
+                className="border-border border p-2 tracking-widest font-serif rounded-md hover:bg-border"
               >
-                Demo
-              </Link>
-              <Link
-                href={item.githubUrl}
-                target="_blank"
-                className="border-border border p-2 rounded-md hover:bg-border"
-              >
-                Github
+                More Info
               </Link>
             </div>
           </div>
