efc1d1e35d0270eb5e6bd220f7b07718f779ee79
muzaffer <muzafferckaplan@gmail.com>
2023-01-21 14:20:55 +0300

    skills page added anımatıon

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,9 +1,21 @@
+"use client";
+import { motion } from "framer-motion";
 import { skillsIcon } from "../../data/skillsIconModal";
 import { skillsIconModal } from "../../typings";
 
 const Skills = () => {
   return (
-    <div className="text-center flex flex-col">
+    <motion.div
+      initial={{ x: 500, opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      exit={{ x: 300, opacity: 0 }}
+      transition={{
+        type: "spring",
+        stiffness: 260,
+        damping: 20,
+      }}
+      className="text-center flex flex-col"
+    >
       <p className="block md:hidden text-2xl mb-4">Skills</p>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-y-2 md:gap-y-10 gap-x-20 self-center lg:grid-cols-5 md:ml-36 lg:ml-20 flex-wrap">
         {skillsIcon.map((item: skillsIconModal) => (
@@ -13,13 +25,13 @@ const Skills = () => {
           >
             {<item.icon size={60} color={item.color} />}
             <div className="text-xl absolute hidden group-hover:flex w-24 h-24 md:w-28 md:h-28 lg:w-32 lg:h-32  flex-col justify-center items-center rounded-full bg-thirty">
-              <p>{item.percent}%</p>
+              {/* <p>{item.percent}%</p> */}
               <p>{item.title}</p>
             </div>
           </div>
         ))}
       </div>
-    </div>
+    </motion.div>
   );
 };
 
