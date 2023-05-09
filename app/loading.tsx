dcee5406194b0c3758c5760b8a8e00b80f181f50
muzaffer <muzafferckaplan@gmail.com>
2023-05-10 00:17:23 +0300

    changed loading screen

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,12 @@
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+
 const Loading = () => {
-  return <div className="text-3xl">Loading</div>;
+  return (
+    <div className="flex justify-center text-3xl">
+      <AiOutlineLoading3Quarters size={36} className="mr-5 animate-spin" />
+      Loading...
+    </div>
+  );
 };
 
 export default Loading;
